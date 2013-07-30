113fda2ca131c39a335bba2546fd67718ce5654a
Collin Price <collin.price@gmail.com>
2013-07-30 17:38:45 -0400

    Tested.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
 
+	std::vector<std::string> files = IOHelper::contentsOfFolder("xyz");
+
+	for (std::vector<std::string>::iterator file = files.begin(); file != files.end(); ++file) {
+		
+		std::cout << *file << std::endl;
+	}
 
 	return 0;
 }
